dad9e99b84dc25cfd8a4277eaa2cacccee57adcc
kien_hitek <dangvmse63447@fpt.edu.vn>
2024-03-02 01:37:19 +0700

    update: booking flow

--- Be/src/main/java/com/swp/repositories/BookingRepository.java
+++ Be/src/main/java/com/swp/repositories/BookingRepository.java
@@ -13,13 +13,13 @@ import java.util.List;
 public interface BookingRepository extends JpaRepository<Booking, Integer>, JpaSpecificationExecutor<Booking> {
     Booking findByCustomerUsIdAndPackagesId(Integer customer, Integer packages);
 
-    @Query("SELECT r FROM Booking r WHERE r.startTime >= ?1 AND r.endTime <= ?2 AND r.status = 'APPROVED'")
+    @Query(value = "SELECT r FROM Booking r WHERE r.startTime >= ?1 AND r.endTime <= ?2 AND r.status = 'APPROVED'")
     List<Booking> findAllApprovedReservationsInTime(Date startTime, Date endTime);
 
     // This is a custom query that finds all reservation has startTime and endTime in the same day as the given date
     @Query("SELECT r FROM Booking r WHERE DATE(r.startTime) = :date")
     List<Booking> findAllByDate(@Param("date") Date date);
-    @Query(value = "SELECT * FROM Booking r WHERE r.end_time >= ?1 and r.start_time <= ?2 and r.package_id = ?3" , nativeQuery = true)
+    @Query(value = "SELECT * FROM booking r WHERE r.end_time >= ?1 and r.start_time <= ?2 and r.package_id = ?3" , nativeQuery = true)
     List<Booking> findByDateRangeOverlap(Date startTime, Date endTime , Integer packageId);
     @Query(value = "SELECT * " +
             "FROM booking b " +
